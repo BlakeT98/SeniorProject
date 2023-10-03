f4e11779617ff88e835ee2dcd833b823ad2881b2
BlakeT98 <80071090+BlakeT98@users.noreply.github.com>
2023-10-03 11:50:47 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,13 +1,17 @@
 
+//Creates Map
 function myMap(){
   var coord = {
     center:new google.maps.LatLng(46.547581, -87.395592),
     zoom:16,
   };
   var map = new google.maps.Map(document.getElementById('map'), coord);
+
+  //Creates marker pin
   // var marker = new google.maps.Marker({  position: coord, map: map }); 
 }
-  
+
+//Creates button
 function onButtonClick() {
   alert('Button clicked!');
 }
@@ -15,10 +19,12 @@ function onButtonClick() {
 const button = document.querySelector('button');
 button.addEventListener('click', onButtonClick);
 
-const newButton = document.createElement('button');
+//Creates button at bottom of html page
+/*const newButton = document.createElement('button');
 newButton.textContent = 'Click me!';
 document.body.appendChild(newButton);
 
 newButton.addEventListener('click', () => {
   alert('New button clicked!');
 });
+*/
