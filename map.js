2a99a42f2c1ebb74ace99e9d68d5192ff6cd585c
BlakeT98 <80071090+BlakeT98@users.noreply.github.com>
2023-10-23 13:32:43 -0400

    Updating enter button map.js

--- map.js
+++ map.js
@@ -33,22 +33,22 @@ function myMap(){
 
 //Applies onButtonClick to button
 const button = document.querySelector('button');
-//button.addEventListener('click', grabAddress);
-button.addEventListener('click', onButtonClick);
+button.addEventListener('click', grabAddress);
+//button.addEventListener('click', onButtonClick);
 
 //Grabs addresses from input text
-/*function grabAddress(){
+function grabAddress(){
  var start = document.getElementById("start").value;
  var end = document.getElementById("end").value;
  alert(start + ' ' + end);
- */
+ 
 }
 
 
 //Creates pop up message at button click
-function onButtonClick() {
+/*function onButtonClick() {
   alert('Button clicked!');
-}
+}*/
 
 //Creates button at bottom of html page
 /*const newButton = document.createElement('button');
