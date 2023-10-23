a48e6c46000e0a96b3841aa058b4273b8e1b74c5
BlakeT98 <80071090+BlakeT98@users.noreply.github.com>
2023-10-23 14:17:13 -0400

    cleaning up map.js

--- map.js
+++ map.js
@@ -1,23 +1,3 @@
-//Creates objects that are nodes in a graph and connects nodes to other nodes via edges
-/*class Graph{
- 
-}
-class Node{
-  String Name;
-  Node North = new Node();
-  Node East = new Node();
-  Node South = new Node();
-  Node West = new Node();
-  int Elevation;
-  Node(String name, Node n, Node e, Node s, Node w, int ele){
-    this.Name = name;
-    this.North = n;
-    this.East = e;
-    this.South = s;
-    this.West = w;
-    this.Elevation = ele;
-  }
-}*/
 
 //Creates Map
 function myMap(){
