90ff502215984ed008ad430b86232c6bfb851f1f
BlakeT98 <80071090+BlakeT98@users.noreply.github.com>
2023-11-08 13:02:37 -0500

    adding bold map.js

--- map.js
+++ map.js
@@ -9,7 +9,7 @@ function myMap(){
   });
    // Create the initial InfoWindow.
   let infoWindow = new google.maps.InfoWindow({
-    content: "<h2 id=\"pattern-layout\">Click the map to get Lat/Lng!</h2>",
+    content: "<h2 id=\"pattern-layout\"><b>Click the map to get Lat/Lng!</b></h2>",
     position: myLatlng,
     
   });
