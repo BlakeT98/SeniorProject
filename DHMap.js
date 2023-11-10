9a9beaf27235dc865596f849741361de26ad2c18
BlakeT98 <80071090+BlakeT98@users.noreply.github.com>
2023-11-10 08:39:28 -0500

    Update DHMap.js

--- DHMap.js
+++ DHMap.js
@@ -2,7 +2,7 @@
 let addresses = "100 Peter White Dr,2600 N Lakeshore Blvd,100 Hawley St,100 N 3rd St,300 E Michigan St,200 N Front St,200 N 3rd St,200 E Michigan St";
 const addyArray = addresses.split(",");
 let temp = "";
-for(let i = 0; i < addArray.length; i++){
+for(let i = 0; i < addyArray.length; i++){
   temp += addyArray[i] + "<br>";
 }
 document.getElementById("test").innerHTML = temp;
