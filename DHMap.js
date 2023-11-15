e80a01f875437f2e8fc7358a0475f4d094be7c10
BlakeT98 <80071090+BlakeT98@users.noreply.github.com>
2023-11-14 19:55:28 -0500

    Update DHMap.js

--- DHMap.js
+++ DHMap.js
@@ -14,8 +14,8 @@ function Intersection(id,north,east,south,west,elev){
   
 //Creating String of addresses seperateed by commas
 //Creating String of intersections seperated by colons
-let addresses = "100  Peter White Dr,2600  N Lakeshore Blvd,100  Hawley St,100  N 3rd St,300  E Michigan St,200  N Front St,200  N 3rd St,200  E Michigan St";
-let intersections = "100  Peter White Dr,2600  N Lakeshore Blvd,100  Hawley St,100  N 3rd St,:100  N 3rd St,300  E Michigan St,200  N 3rd St,200  E Michigan St,:null  ,null  ,100  N 3rd St,null  ,:";
+let addresses = "100 Peter White Dr,2600 N Lakeshore Blvd,100 Hawley St,100 N 3rd St,300 E Michigan St,200 N Front St,200 N 3rd St,200 E Michigan St";
+let intersections = "100 Peter White Dr,2600 N Lakeshore Blvd,100 Hawley St,100 N 3rd St,:100 N 3rd St,300 E Michigan St,200 N 3rd St,200 E Michigan St,:null,null,100 N 3rd St,null,:";
 
 //splitting addresses into an array
 const addyArray = addresses.split(",");
@@ -29,12 +29,16 @@ let blck;
 let n;
 //Creating Streets[] with addyArray
 for(let i = 0; i < addyArray.length; i++){
-  if(addyArray[i].charAt(3) == " ")blck = addyArray[i].substring(0,3);
-  else blck = addyArray[i].substring(0,4);
-  n = addyArray[i].substring(5);
+  if(addyArray[i].charAt(3) == " "){
+    blck = addyArray[i].substring(0,3);
+    n = addyArray[i].substring(4);
+  }
+  else {
+    blck = addyArray[i].substring(0,4);
+    n = addyArray[i].substring(5);
+  }
   Streets[i] = new Street(blck,n);
-  //temp += Streets[i].Name + "<br>";
-  
+  //temp += Streets[i].Name + "<br>"; 
 }
 //temp += interArray[0] + "<br>";
 //Creating Intersections[] with interArray
@@ -54,12 +58,40 @@ for(let j = 0; j < interArray.length-1; j++){
   //temp += Intersections[j].Id + ", North: " + Intersections[j].North + ", East: " + Intersections[j].East + ", South: " + Intersections[j].South + ", West: " + Intersections[j].West + ", Elevation: " + Intersections[j].Elevation + "<br>";
 }
 
-function searchAddress(){
-  for(let i = 0; i < interArray.length-1; i++){
-      temp += Intersections[i].Id + ", North: " + Intersections[i].North + ", East: " + Intersections[i].East + ", South: " + Intersections[i].South + ", West: " + Intersections[i].West + ", Elevation: " + Intersections[i].Elevation + "<br>";
+function searchAddress(street){
+  let block = 0;
+  let st = "";
+  let inner = 0;
+  let outer = 0;
+  for(let i = 0; i < street.length(); i++){
+    if(street.charAt(3) == " "){
+      block = street.substring(0,3);
+      st = street.substring(4);
+    }
+    else {
+      block = street.substring(0,4);
+      st = street.substring(5);
+    }
   }
+  for(let j = 0; j < Streets.length; j++){
+    if(st == Streets[j].Name){
+      if(inner == 0 || outer == 0){
+        if(block >= Streets[j].Block)innner = Streets[j].Block;
+        else if(block <= Streets[j].Block)outer = Streets[j].Block;
+      }
+      else{
+        if(inner < Streets[j].Block && Streets[j].Block < block)inner = Streets[j].Block;
+        else if(outer > Streets[j].Block && Streets[j].Block > block)outer = Streets[j].Block;
+      }
+    }
+  }
+  //inner is the correct block number for matching street name
+  let s = "" + inner + " "  + st;
+ for(int k = 0; k < Intersections.length; k++){
+   if(s == Intersections[k].North || s == Intersections[k].East || s == Intersections[k].South || s == Intersections[k].West)return Intersections[k].Id;
+ }
 }
 
-searchAddress();
+//let found = searchAddress("123 N 3rd St");
 
 document.getElementById("test").innerHTML = temp;
