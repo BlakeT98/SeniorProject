c316e8bcd2e5edf1097a7ef4920a38b83d360d1c
BlakeT98 <80071090+BlakeT98@users.noreply.github.com>
2023-12-05 19:08:08 -0500

    Update DHMap.js

--- DHMap.js
+++ DHMap.js
@@ -415,6 +415,7 @@ function findFinish(i,e){  //Parameters are array of ids and elevations
   }
   if(len == 2){
     let found = Math.min(e[0], e[1]);
+    console.log("TEST2 " + e[0] + " " + e[1]);
     for(let j = 0; i < len; i++){
       if(e[j] == found) {
         console.log("TEST: " + e[j] + " AND " + found);
