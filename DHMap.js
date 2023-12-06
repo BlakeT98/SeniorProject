42454f556245f0bc138ddaa35bc736c6852fbee1
BlakeT98 <80071090+BlakeT98@users.noreply.github.com>
2023-12-05 19:14:00 -0500

    Update DHMap.js

--- DHMap.js
+++ DHMap.js
@@ -415,8 +415,8 @@ function findFinish(i,e){  //Parameters are array of ids and elevations
   }
   if(len == 2){
     let found = Math.min(e[0], e[1]);
-    console.log("TEST2 " + e[0] + " " + e[1]);
-    console.log("TEST3: " + found);
+    //console.log("TEST2 " + e[0] + " " + e[1]);
+    //console.log("TEST3: " + found);
     for(let j = 0; i < len; i++){
       if(e[j].equals(found)) {
         console.log("TEST: " + e[j] + " AND " + found);
@@ -429,7 +429,7 @@ function findFinish(i,e){  //Parameters are array of ids and elevations
   if(len == 3){
     let found = Math.min(e[0], e[1], e[2]);
     for(let j = 0; i < len; i++){
-      if(e[j] == found) {
+      if(e[j].equals(found)) {
         path += Intersections[i[j]].Coord;
         foundID = i[j];
       }
@@ -439,7 +439,7 @@ function findFinish(i,e){  //Parameters are array of ids and elevations
   if(len == 4){
     let found = Math.min(e[0], e[1],e[2],e[3]);
     for(let j = 0; i < len; i++){
-      if(e[j] == found) {
+      if(e[j].equals(found)) {
         path += Intersections[i[j]].Coord;
         foundID = i[j];
       }
