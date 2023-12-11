5dcfd8ea783776221863fb4228d0115280db12ad
BlakeT98 <80071090+BlakeT98@users.noreply.github.com>
2023-12-11 11:45:16 -0500

    Update map.js

--- map.js
+++ map.js
@@ -57,6 +57,7 @@ window.myMap = myMap;
 
 function displayRoute(directionsService, directionsRenderer){
   var p = document.getElementById("path").value;
+  console.log.("PATH::::: " + p);
   const paths = p.split(":");
   
   directionsService
