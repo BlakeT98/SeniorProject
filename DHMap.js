820898c190f3bc5506bfa801907f4f120ca2b4df
BlakeT98 <80071090+BlakeT98@users.noreply.github.com>
2023-12-13 20:25:24 -0500

    Update DHMap.js

--- DHMap.js
+++ DHMap.js
@@ -44,7 +44,7 @@ for(let i = 0; i < addyArray.length; i++){
   //temp += Streets[i].Name + "<br>"; 
 }
 //temp += interArray[0] + "<br>";
-console.log(interArray[42]);
+console.log("7 " + interArray[7] + " | 8 " + interArray[7] + " | 9 " + interArray[7] + " | 10 " + interArray[7]);
 //Creating Intersections[] with interArray
 for(let j = 0; j < interArray.length-1; j++){
   const compass = interArray[j].split(",");
